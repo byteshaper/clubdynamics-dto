3c6fde792d53488e3ccc72ec30517f7adc9675ab
Byteshaper <henning@byteshaper.com>
2018-01-13 14:42:37 +0100

    started subpackaging; added email contact type to user

---
+++ src/main/java/com/clubdynamics/dto/user/UserCreateDto.java
@@ -0,0 +1,14 @@
+package com.clubdynamics.dto.user;
+
+import com.clubdynamics.dto.contact.ContactTypeDto;
+
+public class UserCreateDto {
+  
+  public String username;
+  
+  public String password;
+  
+  public String email;
+  
+  public ContactTypeDto emailContactType;
+}
