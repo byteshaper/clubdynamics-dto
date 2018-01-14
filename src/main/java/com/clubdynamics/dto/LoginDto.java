3f492777b0e1884525d3cc13f1256cffc5fcecb8
Byteshaper <henning@byteshaper.com>
2018-01-14 13:05:43 +0100

    added javax.validation annotations to DTOs

--- src/main/java/com/clubdynamics/dto/LoginDto.java
+++ src/main/java/com/clubdynamics/dto/LoginDto.java
@@ -1,31 +1,19 @@
 package com.clubdynamics.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class LoginDto {
   
-  private String username;
+  @NotBlank
+  public String username;
   
-  private String password;
+  @NotBlank
+  public String password;
   
   public LoginDto() {}
   
   public LoginDto(String username, String password) {
     this.username = username;
-    this.password = password;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
     this.password = password;
   }
 }
